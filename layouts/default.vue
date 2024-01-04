78a68082e7e621d48bb7a0abc7a33964991d61b1
breakage <dev.iosync@gmail.com>
2024-01-05 01:03:01 +0300

    Creates default page layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<NuxtLink to="/">Home</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/about">About</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink :to="{ name: 'products' }">Products</NuxtLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+		<div>
+			<slot />
+		</div>
+
+		<footer>
+			<div>Copyright IOSync @ 2024</div>
+		</footer>
+	</div>
+</template>
+
+<script setup></script>
