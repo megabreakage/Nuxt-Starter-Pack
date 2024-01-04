95df86ed2ddf1704c1d7cdcd1f98d2651226892f
breakage <dev.iosync@gmail.com>
2024-01-05 01:18:47 +0300

    Adds bootstrap

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,11 +3,13 @@
         <h2>List of all products</h2>
         <p>
             <ul>
-            <li> <NuxtLink to="product/coffee-table"> Product 1</NuxtLink> </li>
-            <br />
-            <li> <NuxtLink to="product/google-pixel-6a"> Product 2</NuxtLink> </li>
-            <br />
-            <li> <NuxtLink to="product/iphone-15-pro-max"> Product 3</NuxtLink> </li>
+                <li> <NuxtLink to="products/coffee-table"> Product 1</NuxtLink> </li>
+                <br />
+                <li> <NuxtLink to="products/google-pixel-6a"> Product 2</NuxtLink> </li>
+                <br />
+                <li> <NuxtLink to="products/iphone-15-pro-max"> Product 3</NuxtLink> </li>
+                <br />
+                <li> <NuxtLink :to="products/sony-bravia"> Product 4</NuxtLink> </li>
             </ul>
         </p>
     </div>
