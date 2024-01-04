91ddfa2dd53750fa511ebcfd4bb86f014f659006
breakage <dev.iosync@gmail.com>
2024-01-05 01:07:26 +0300

    Adds product pages

---
+++ pages/products/[slug].vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<p>Product ID is: {{ slug }}</p>
+	</div>
+</template>
+
+<script setup>
+	const { slug } = useRoute().params;
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>List of all products</h2>
+        <p>
+            <ul>
+            <li> <NuxtLink to="product/coffee-table"> Product 1</NuxtLink> </li>
+            <br />
+            <li> <NuxtLink to="product/google-pixel-6a"> Product 2</NuxtLink> </li>
+            <br />
+            <li> <NuxtLink to="product/iphone-15-pro-max"> Product 3</NuxtLink> </li>
+            </ul>
+        </p>
+    </div>
+</template>
+
+<script setup>
+const { slug } = useRoute().params;
+</script>
